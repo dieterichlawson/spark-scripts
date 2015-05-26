ea42118e728b69e78579e03efe9cbe1c63a38f38
Dieterich Lawson <dieterich.lawson@gmail.com>
2015-05-26 14:45:50 -0700

    Undoing override

--- launch_cluster.sh
+++ launch_cluster.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd ~/dev/spark/ec2 
-./spark-ec2 -k spark -i ~/Downloads/spark.pem \
-                --instance-type=m3.xlarge \
-                --slaves=8 \
-                --spark-version=1.0.0 \
-                launch lassotest
+./spark-ec2 --key-pair=spark \
+            --identity-file=/Users/dlaw/Downloads/spark.pem \
+            --instance-type=m3.xlarge \
+            --slaves=2 \
+            --spark-version=1.3.1 \
+            --region=us-west-1 \
+            --zone=us-west-1a \
+            launch distlp2
